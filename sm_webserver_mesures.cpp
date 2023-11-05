5f03ccf8a8e2019ae365f1e01dfced3fa2187af9
Claude VIEVILLE <claude.vizville@univ-lille.fr>
2023-11-05 16:17:15 +0100

    Correction du getMesures - fonctionne à présent avec les deux paramètres from et to

--- sm_webserver_mesures.cpp
+++ sm_webserver_mesures.cpp
@@ -1,9 +1,9 @@
 #include "sm_webserver_mesures.h"
 
 // les variables globales pour l'envoi des mesures stockées sur la carte
-int from = 1;
-int to= 100;
-int numLigne=0;
+unsigned long _from = 1;
+unsigned long  _to= 100;
+unsigned long  numLigne=0;
 size_t len=0;
 size_t pos=0;
 File file;
@@ -26,44 +26,38 @@ if(serverBusy){
     setup_File();
     file = SD_MMC.open("/mesures.csv", FILE_READ);
     numLigne=0;
-    from=getNextIdMesure()-1;
-    to=-1;
+    _from=getNextIdMesure()-1;
+    _to=-1;
     if(request->hasParam("from")){
-      from=request->getParam("from")->value().toInt();
+      _from=request->getParam("from")->value().toInt();
     }
     if(request->hasParam("to")){
-      to=request->getParam("to")->value().toInt();
+      _to=request->getParam("to")->value().toInt();
     }
-    if(from<0){from=0;}
-    if(to<0||to<from){ to=-1;} 
+    if(_from<0){_from=0;}
+    if(_to<0||_to<_from){ _to=-1;} 
     String s ="";
 
-
-    while(file.available()&&numLigne<from){
+    while(file.available()&&numLigne<_from){
       s=file.readStringUntil('\n');
       pos+=s.length()+1;
       numLigne++;
     }
-    if(to!=-1){
-      while(file.available()&&numLigne<=to){
+    if(_to==-1){
+      len=0;
+    } else {
+      while(file.available()&&numLigne<=_to){
         s=file.readStringUntil('\n');
         numLigne++;
         len+=s.length()+1;
      } 
-    } else {
-      len=-1;
     }
 
-    Serial.print("from= ");Serial.println(from);
-    Serial.print("to= ");Serial.println(to);
-    Serial.print("len= ");Serial.println(len);
-    Serial.print("pos= ");Serial.println(pos);
     file.seek(pos);
     
     AsyncWebServerResponse *response = request->beginChunkedResponse("text/plain", [](uint8_t *buffer, size_t maxLen, size_t index) -> size_t {
       // il y a un to
-      if (file.available()&&len>=0){
-        Serial.print("len= ");Serial.println(len);
+      if (file.available()&&_to>0&&len>0){
         if(len<maxLen) {
           size_t l=len;
           len=0;
@@ -72,20 +66,19 @@ if(serverBusy){
           return r;
         } else if(len>=maxLen){
           len-=maxLen;
-          if(len<0) len=0;
           size_t r= file.read(buffer, maxLen);
           if(r==0) _close();
           return r;
         } 
       }
       // pas de to
-      if (file.available()&&len<0){
+      if (file.available()&&_to==-1&&len==0){
         size_t r= file.read(buffer, maxLen);
         if(r==0) _close();
         return r;
       }
-
-      
+     _close();
+     return 0; 
       });
       return response;
 }
